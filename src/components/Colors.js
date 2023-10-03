0eed5e366f0c3000ccdd6b297b6314314b328ee0
eddiemuhoro <eddiemuhoro@gmail.com>
2023-10-03 16:14:39 +0300

    (fix) xolor scale count

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -6,38 +6,9 @@ const Colors = () => {
     const [ndreColors, setNdriColors] = useState([]); // Initialize an empty array
     const [msaviColors, setMsaviColors] = useState([]); // Initialize an empty array
     const [ndwiColors, setNdwiColors] = useState([]); // Initialize an empty array
-    const colorScale = d3
-    .scaleSequential(d3.interpolateViridis)
-    .domain([-1, 1]);
-
-    // Define a custom color scale for NDMI (bluish colors)
-const ndmiColorScale = d3
-.scaleSequential(d3.interpolateBlues) // Use a blue-based color scale
-.domain([-0.3, 1]);
-
-// Define a custom color scale for NDVI (green to red)
-const ndviColorScale = d3
-.scaleSequential(d3.interpolateRdYlGn) // Use a green-to-red color scale
-.domain([0, 1]);
-
-const ndreColorScale = d3
-.scaleSequential(d3.interpolateRdYlGn) // Red to green color scale
-.domain([-1, 1]); // Adjust the domain based on your data range
-
-const msaviColorScale = d3
-.scaleSequential(d3.interpolateRdYlGn) // Brown to green color scale
-.domain([-1, 1]); // Adjust the domain based on your MSAVI data range
-
-const ndwiColorScale = d3
-.scaleSequential(d3.interpolateBlues) // Blue color scale
-.domain([-1, 1]); 
-
-const etColorScale = d3
-.scaleSequential(d3.interpolateYlOrRd) // Yellow to red color scale
-.domain([0, 1]); // Adjust the domain based on your ET data range
 
 useEffect(() => {
-    const numColors = 10; // Adjust the number of colors as needed
+    const numColors = 20; // Adjust the number of colors as needed
     const ndwiColorScale = d3
       .scaleSequential(d3.interpolateBlues)
       .domain([-1, 1]);
@@ -53,7 +24,7 @@ useEffect(() => {
   }, []); // Re-run this effect whenever ndwiData changes
 
     useEffect(() => {
-        const numColors = 10; // Adjust the number of colors as needed
+        const numColors = 20; // Adjust the number of colors as needed
         const ndreColorScale = d3
           .scaleSequential(d3.interpolateRdYlGn)
           .domain([-1, 1]);
@@ -70,7 +41,7 @@ useEffect(() => {
         , []); // Re-run this effect whenever ndriData changes
 
         useEffect(() => {
-            const numColors = 10; // Adjust the number of colors as needed
+            const numColors = 20; // Adjust the number of colors as needed
             const msaviColorScale = d3
               .scaleSequential(d3.interpolateRdYlGn)
               .domain([-1, 1]);
@@ -87,7 +58,7 @@ useEffect(() => {
             , []); // Re-run this effect whenever ndriData changes
 
             useEffect(() => {
-                const numColors = 10; // Adjust the number of colors as needed
+                const numColors = 20; // Adjust the number of colors as needed
                 const ndwiColorScale = d3
                   .scaleSequential(d3.interpolateBlues)
                   .domain([-1, 1]);
@@ -115,17 +86,7 @@ useEffect(() => {
             gridGap: 20
         }}
         >
-        <div>
-            {colors.map((color, i) => (
-                <>
-               
-                <div key={i} style={{ backgroundColor: color, width: 150, height: 50 }}>{color}</div>
-
-                </>
-            ))}
-        </div>
-
-        <div>
+              <div>
             {ndreColors.map((color, i) => (
                 <>
                
@@ -145,6 +106,17 @@ useEffect(() => {
             ))}
         </div>
 
+        <div>
+            {colors.map((color, i) => (
+                <>
+               
+                <div key={i} style={{ backgroundColor: color, width: 150, height: 50 }}>{color}</div>
+
+                </>
+            ))}
+        </div>
+
+      
         <div>
             {ndwiColors.map((color, i) => (
                 <>
