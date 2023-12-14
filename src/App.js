43aa5a9308cb3547ea0f42a115512267a7ff8756
eddiemuhoro <eddiemuhoro@gmail.com>
2023-12-14 14:32:04 +0300

    (add) web cam

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Webcam from 'react-webcam';
 import './App.css';
 import Colors from './components/Colors';
+import CustomWebcam from './components/WebCam';
 
 function App() {
   return (
     <div className="App">
-      <Colors />
+      {/* <Colors /> */}
+      <CustomWebcam />
     </div>
   );
 }
